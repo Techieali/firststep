0df03410b19ef31bfcd62db2bc44ebb34ed0aee6
Ali <alimahroof09@gmail.com>
2021-03-01 16:27:19 +0530

    tflite mobile application

---
+++ mobile/lib_interpreter/src/main/java/org/tensorflow/lite/examples/detection/tflite/TFLiteObjectDetectionAPIModel.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:8d45e584b8066899711d28a2d61bf0cfbda7886f7ce768936fb1d0e6d9bf22b8
+size 10115
---
+++ mobile/lib_task_api/src/main/java/org/tensorflow/lite/examples/detection/tflite/TFLiteObjectDetectionAPIModel.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:b065cd6efa90241efbd9c70233e02a11591b557786739d9e48066c6827d0fc7b
+size 6014
